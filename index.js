b9d3ca0712e4f437ece37c9294fd84188fb9162f
Mosawkat <109388171+Mosawkat@users.noreply.github.com>
2023-10-24 10:48:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,92 +1,71 @@
 let history = [];
-let H = '';
-let output;
-let prevNum;
-let PN;
-let currentNum;
-let clearNum;
-let add, sub, mul, div;
-function initialize()
-{
-    document.getElementById('allNums').innerHTML="0";
-}
-function resetCurrentNum()
-{
-    currentNum = undefined;
-    CN = document.getElementById("allNums");
-    CN.innerHTML = "0";
+let currentNum = '';
+let operator = '';
+let result = 0;
 
+function initialize() {
+  clearDisplay();
 }
 
-//Need to add all elements to the history array, then when total button is clicked,
-//a loop will loop through the array and calculate based on the contents in the array.
+function clearDisplay() {
+  currentNum = '';
+  document.getElementById('allNums').innerHTML = '0';
+}
 
-function buttonPressed(ID)
-{
-    CN = document.getElementById("allNums");
-    document.getElementById('AC').innerHTML = 'C';
-    if (ID=="AC")
-    {
-        if (typeof currentNum !=='undefined')
-        {
-            resetCurrentNum();
-            CN.innerHTML;
-        }
-        else if (typeof currentNum=='undefined')
-        {
-            document.getElementById('AC').innerHTML = 'C';
-            document.getElementById('AC').innerHTML = 'AC';
-        }
+function updateDisplay() {
+  document.getElementById('allNums').innerHTML = currentNum;
+}
 
-    }
-    if (ID=="7")
-    {
-        currentNum = ID;
-        CN.innerHTML= H;
-        if (currentNum==prevNum)
-        {
-            H+=ID;
-            prevNum = ID;
-            currentNum = parseInt(H, 10);
+function buttonPressed(ID) {
+  switch (ID) {
+    case 'AC':
+      clearDisplay();
+      break;
+    case 'PM':
+      // Implement code for changing the sign (+/-)
+      break;
+    case 'Percent':
+      // Implement code for percentage (%)
+      break;
+    case 'Div':
+      operator = '/';
+      history.push(currentNum, operator);
+      clearDisplay();
+      break;
+    case 'Multiply':
+      operator = '*';
+      history.push(currentNum, operator);
+      clearDisplay();
+      break;
+    case 'Subtraction':
+      operator = '-';
+      history.push(currentNum, operator);
+      clearDisplay();
+      break;
+    case 'Addition':
+      operator = '+';
+      history.push(currentNum, operator);
+      clearDisplay();
+      break;
+    case 'Equal':
+      if (operator && currentNum !== '') {
+        history.push(currentNum);
+        result = eval(history.join(' '));
+        currentNum = result.toString();
+        operator = '';
+        history = [];
+        updateDisplay();
+      }
+      break;
+    default:
+      if (ID >= '0' && ID <= '9') {
+        if (currentNum === '0') {
+          currentNum = ID;
+        } else {
+          currentNum += ID;
         }
-        else
-        {
-            H+=ID;
-            prevNum = ID;
-            currentNum = parseInt(H, 10);
-            CN.innerHTML= H;
-        }
-
-    }
-
-    if (ID=="8")
-    {
-        currentNum = ID;
-        CN.innerHTML= ID;
-        if (currentNum==prevNum)
-        {
-            H+=ID;
-            prevNum = ID;
-            currentNum = parseInt(H, 10);
-        }
-        else
-        {
-            H+=ID;
-            prevNum = ID;
-            currentNum = parseInt(H, 10);
-            CN.innerHTML= H;
-        }
-    }
-    if (ID=="Multiply")
-    {
-        mul = true;
-    }
-    if (ID=="Equal")
-    {
-        if (mul==true)
-        {
-            output = currentNum * parseInt(H, 10);
-            CN.innerHTML = output;
-        }
-    }
+        updateDisplay();
+      }
+      break;
+  }
 }
